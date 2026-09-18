7b1290fdda9ab9e569059434d6a69471193cf2d6
kholyo18 <khaledfoll12@gmail.com>
2026-09-18 13:29:13 +0200

    Add account deletion request and retention surfaces

--- functions/index.js
+++ functions/index.js
@@ -205,3 +205,14 @@ exports.completeSignupProfile = onCall({region:'europe-west1',timeoutSeconds:60}
 const { createSessionBootstrapHandler } = require('./security/session-bootstrap');
 exports.markCurrentSession = onCall({region:'europe-west1',timeoutSeconds:60},
   createSessionBootstrapHandler({auth:getAuth(),db:getFirestore()}));
+
+
+const { createAccountDeletionRequestHandler } = require('./security/account-deletion-request');
+exports.requestAccountDeletion = onCall(
+  { region: 'europe-west1', timeoutSeconds: 60 },
+  createAccountDeletionRequestHandler({
+    auth: getAuth(),
+    db: getFirestore(),
+    FieldValue,
+  }),
+);
